c780778533f45a5014f4cce2a671a83a1b606135
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-11 00:58:35 +0300

    User routes Impl Including Post and Get

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../app/models/user')
+
+/* GET users listing. */
+router.route('/')
+  .get( async(req,res) => {
+    const user = await User.findAll();
+    res.status(200).send(user);
+  })
+
+  .post( async (req,res) => {
+    const { name, email, password} = req.body;
+    const user = await User.create({
+      name: name,
+      email:email,
+      password: password
+    })
+    if(user) return res.status(201).send(user);
+    res.status(500).send("Something went wrong please try again later");
+  })
+module.exports = router;
