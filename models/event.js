9e0df1428b98bee01b680f3d47dde01c58609102
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-15 23:27:48 +0300

    Models Insertions

---
+++ models/event.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Event.belongsTo(models.User)
+    }
+  }
+  Event.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    UserId: {
+      type:DataTypes.INTEGER,
+      references: {
+        model: {
+          tableName: 'Users'
+        },
+        key: 'id'
+      },
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Event',
+  });
+  return Event;
+};
