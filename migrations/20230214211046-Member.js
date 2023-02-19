6e076c9bcc8dfd454b3d7b62433bd02c75dc2f38
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-19 22:58:14 +0300

    Migrations Fix Add Email:UNIQUE ,FIx foreign Keys

--- migrations/20230214211046-Member.js
+++ migrations/20230214211046-Member.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('members',{
+    await queryInterface.createTable('Members',{
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -42,6 +42,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-      await queryInterface.dropTable('members');
+      await queryInterface.dropTable('Members');
   }
 };
